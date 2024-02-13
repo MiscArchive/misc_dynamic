9728f7af6118608412c9a60ce79986455a9860f0
BRISTO <brestosaji@gmail.com>
2024-02-13 23:14:28 +0530

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -181,7 +181,7 @@
       <div class="item">
         <figure><img src="{{ asset('assets/images/math.jpg') }}" alt="Image"></figure>
     </div>
-    <!-- end item -->
+    
     <div class="item">
       <figure><img src="{{ asset('assets/images/ananthapuri.jpg') }}" alt="Image"></figure>
   </div>
